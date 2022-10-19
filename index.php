eef323b0f7c5c196d2887a6064d03f693ef62c4c
PM-Primo <pierrem.primo@gmail.com>
2022-10-19 16:48:06 +0200

    Debug unnecessary horizontal scrollbar

--- index.php
+++ index.php
@@ -12,63 +12,62 @@
     <title>Ajout produit</title>
 </head>
 <body>
-    <div class="ajoutProduit">
-        <h1>Ajouter un produit</h1>
-        <form action="traitement.php?action=add" method="post"> <!-- action="traitement.php" permet d'appeler la page traitement.php au moment du clic sur submit //  POST car formulaire -->
-            <p class="elementForm">
-                <label>
-                    Nom du produit :<br>
-                    <input type="text" name="name" class="champTxt">
-                </label>
-            </p>
-            <p class="elementForm">
-                <label>
-                    Prix du produit :<br>
-                    <input type="number" step="any" name="price" class="champTxt" min="0">
-                </label>
-            </p>
-            <p class="elementForm">
-                <label>
-                    Quantité désirée :<br>
-                    <input type="number" name="qtt" value="1" class="champTxt" min="1">
-                </label>
-            </p>
-            <p class="elementForm">
-                <label>
-                    Description du produit :<br>
-                    <textarea id="description" name="description" rows="6" cols="60" class="champTxt"></textarea>
-                </label>
-            </p>
-            <p>
-                <input type="submit" name="submit" value="Ajouter le produit" class="submitBtn">
-            </p>
-
-        </form>
-
-        <div class="navigation"><a href="recap.php">Vers Recap =></a></div>
-        <div class="compteur">Nb de produits : <?php  
-        
-            if (isset($_SESSION['products'])){
-                echo sizeof($_SESSION['products']); 
-            }
-            else{
-                echo "0";
-            }
-            ?>
-        </div>
+    <div class="wrapper">
+        <div class="ajoutProduit">
+            <h1>Ajouter un produit</h1>
+            <form action="traitement.php?action=add" method="post"> <!-- action="traitement.php" permet d'appeler la page traitement.php au moment du clic sur submit //  POST car formulaire -->
+                <p class="elementForm">
+                    <label>
+                        Nom du produit :<br>
+                        <input type="text" name="name" class="champTxt">
+                    </label>
+                </p>
+                <p class="elementForm">
+                    <label>
+                        Prix du produit :<br>
+                        <input type="number" step="any" name="price" class="champTxt" min="0">
+                    </label>
+                </p>
+                <p class="elementForm">
+                    <label>
+                        Quantité désirée :<br>
+                        <input type="number" name="qtt" value="1" class="champTxt" min="1">
+                    </label>
+                </p>
+                <p class="elementForm">
+                    <label>
+                        Description du produit :<br>
+                        <textarea id="description" name="description" rows="6" cols="60" class="champTxt"></textarea>
+                    </label>
+                </p>
+                <p>
+                    <input type="submit" name="submit" value="Ajouter le produit" class="submitBtn">
+                </p>
+    
+            </form>
+    
+            <div class="navigation"><a href="recap.php">Vers Recap =></a></div>
+            <div class="compteur">Nb de produits : <?php  
             
-    </div>
-
+                if (isset($_SESSION['products'])){
+                    echo sizeof($_SESSION['products']); 
+                }
+                else{
+                    echo "0";
+                }
+                ?>
+            </div>
+                
+        </div>
+    
         <div class="message">
             <?php
-    
                 if(isset($_SESSION['message'])){
                     echo $_SESSION['message'];
                     $_SESSION['message']="";
                 }
-    
             ?>
         </div>
-
+    </div>
 </body>
 </html>
--- recap.php
+++ recap.php
@@ -12,67 +12,65 @@
     <title>Récapitulatif des produits</title>
 </head>
 <body>
-    <?php 
-        if(!isset($_SESSION['products'])|| empty($_SESSION['products'])){ //Si le tableau products rangé dans session est vide 
-            echo"<p class='aucunProduit'>Aucun produit en session ...</p>";
-        }
-        else{
-            echo "<div class='recapProduits'>",
-                    "<h1>Récapitulatif des produits</h1>",
-                    "<table>",
-                        "<thead>",
-                            "<tr>",
-                                "<th>#</th>",
-                                "<th>Nom</th>",
-                                "<th>Description</th>",
-                                "<th>Prix</th>",
-                                "<th>Quantité</th>",
-                                "<th>Total</th>",
-                                "<th>Suppr.</th>",
-                            "</th>",
-                        "</thead>",
-                        "<tbody>";
-                $totalGeneral = 0;
-                foreach($_SESSION['products'] as $index=>$product){ //$index est le numéro du produit & product les tableaux produit avec toutes leurs propriétés
-                    $lienSuppr = "traitement.php?action=delete&id=".$index;
-                    $lienPlus = "traitement.php?action=plus&id=".$index;
-                    $lienMoins = "traitement.php?action=moins&id=".$index;
+    <div id="wrapper">
+        <?php 
+            if(!isset($_SESSION['products'])|| empty($_SESSION['products'])){ //Si le tableau products rangé dans session est vide 
+                echo"<p class='aucunProduit'>Aucun produit en session ...</p>";
+            }
+            else{
+                echo "<div class='recapProduits'>",
+                        "<h1>Récapitulatif des produits</h1>",
+                        "<table>",
+                            "<thead>",
+                                "<tr>",
+                                    "<th>#</th>",
+                                    "<th>Nom</th>",
+                                    "<th>Description</th>",
+                                    "<th>Prix</th>",
+                                    "<th>Quantité</th>",
+                                    "<th>Total</th>",
+                                    "<th>Suppr.</th>",
+                                "</th>",
+                            "</thead>",
+                            "<tbody>";
+                    $totalGeneral = 0;
+                    foreach($_SESSION['products'] as $index=>$product){ //$index est le numéro du produit & product les tableaux produit avec toutes leurs propriétés
+                        $lienSuppr = "traitement.php?action=delete&id=".$index;
+                        $lienPlus = "traitement.php?action=plus&id=".$index;
+                        $lienMoins = "traitement.php?action=moins&id=".$index;
+                        echo "<tr>",
+                                "<td>".$index."</td>",
+                                "<td>".$product["name"]."</td>",
+                                "<td>".$product['description']."</td>",
+                                "<td>".number_format($product["price"],2,",","&nbsp;")."&nbsp;€</td>", //"&nbsp;" correspond à un espace insécable (dans le cas de sauts de ligne, le € reste lié au nombre)
+                                "<td><a href='$lienMoins' class='plusMoins'>-</a> ".$product["qtt"]." <a href='$lienPlus' class='plusMoins'>+</a></td>",
+                                "<td>".number_format($product["total"],2,",","&nbsp;")."&nbsp;€</td>",
+                                "<td><a href='$lienSuppr' class='deleteBtn'>Suppr</a></td>",
+                            "</tr>";
+                        $totalGeneral += $product['total'];
+                    }
                     echo "<tr>",
-                            "<td>".$index."</td>",
-                            "<td>".$product["name"]."</td>",
-                            "<td>".$product['description']."</td>",
-                            "<td>".number_format($product["price"],2,",","&nbsp;")."&nbsp;€</td>", //"&nbsp;" correspond à un espace insécable (dans le cas de sauts de ligne, le € reste lié au nombre)
-                            "<td><a href='$lienMoins' class='plusMoins'>-</a> ".$product["qtt"]." <a href='$lienPlus' class='plusMoins'>+</a></td>",
-                            "<td>".number_format($product["total"],2,",","&nbsp;")."&nbsp;€</td>",
-                            "<td><a href='$lienSuppr' class='deleteBtn'>Suppr</a></td>",
-                        "</tr>";
-                    $totalGeneral += $product['total'];
-                }
-                echo "<tr>",
-                        "<td colspan=4>Total général : </td>", //colspan = pour fusionner des colonnes
-                        "<td><strong>".number_format($totalGeneral,2,",","&nbsp;")."&nbsp;€</strong></td>",
-                    "</tbody>",
-                    "</table>",
-                    "<a href='traitement.php?action=clear' class='deleteAll'>Tout Supprimer</a>",
+                            "<td colspan=4>Total général : </td>", //colspan = pour fusionner des colonnes
+                            "<td><strong>".number_format($totalGeneral,2,",","&nbsp;")."&nbsp;€</strong></td>",
+                        "</tbody>",
+                        "</table>",
+                        "<a href='traitement.php?action=clear' class='deleteAll'>Tout Supprimer</a>",
 
-                "</div>";
-        }
-    
-    ?>
-    <div class="navigation"><a href="index.php" class="toIndex"><= Vers Index</a></div>
-    <div class="compteur">Nb de produits : <?php  
-    
-    if (isset($_SESSION['products'])){
-        echo sizeof($_SESSION['products']); 
-    }
-    else{
-        echo "0";
-    }
+                    "</div>";
+            }
+        
+        ?>
+        <div class="navigation"><a href="index.php" class="toIndex"><= Vers Index</a></div>
+        <div class="compteur">Nb de produits : <?php  
+            if (isset($_SESSION['products'])){
+                echo sizeof($_SESSION['products']); 
+            }
+            else{
+                echo "0";
+            }
+            ?>
+        </div>
 
-    var_dump($_SESSION)
-    ?>
-
-
-</div>
+    </div>
 </body>
 </html>
